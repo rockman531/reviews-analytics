62832618154b2b10f91e99314a08862b1036ba8a
rockman531 <torotoro0214@gmail.com>
2019-08-24 17:46:41 +0800

    add word count

--- read.py
+++ read.py
@@ -1,22 +1,23 @@
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line)
-		count += 1
-		if count % 1000 == 0:
-		    print(len(data))
+    for line in f:
+        data.append(line)
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
 print('檔案讀取完了, 總共有', len(data), '筆資料')
+
 
 sum_len = 0
 for d in data:
-	sum_len += len(d)
+    sum_len += len(d)
 print('留言的平均長度為', sum_len/len(data))
 
 new = []
 for d in data:
-	if len(d) < 100:
-		new.append(d)
+    if len(d) < 100:
+        new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
@@ -24,7 +25,34 @@ print(new[1])
 
 good = []
 for d in data:
-	if 'good' in d:
-		good.append(d)
+    if 'good' in d:
+        good.append(d)
 print('一共有', len(good), '筆留言提到good')
-print(good[0])
+
+
+# 文字記數
+wc = {} # word_count
+for d in data:
+    words = d.split(' ')
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1 #新增key進wc字典
+
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+
+while True:
+    word = input('請問你想查什麼字: ')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, '出現過的次數為: ', wc[word])
+    else:
+    	print('這個字沒有在字典裡喔~~')
+
+print('感謝使用本字典')
+
+
